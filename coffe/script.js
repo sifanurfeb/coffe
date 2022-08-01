b4234f27670d5d1bf38c4b464656c829834b286a
sifanurfeb <85918527+sifanurfeb@users.noreply.github.com>
2022-08-01 15:55:17 +0700

    Add files via upload

---
+++ coffe/script.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active')
+}
+document.querySelector('#close-navbar').onclick = () =>{
+    navbar.classList.remove('active');
+}
